e5a636867e1326a5afc610915cf87f3914ccf714
Anden Wieseler <ttechno@icloud.com>
2021-11-22 23:36:55 +0000

    push for platform switch

--- devices.js
+++ devices.js
@@ -6,12 +6,110 @@ function loadiPhones(page){
         navBlock.querySelector('.button').style.display = 'none'
         includeHTML()
     }
+    if (page == 'a15'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A15 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a15.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a14'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A14 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a14.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a13'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A13 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a13.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a12'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A12 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a12.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a11'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A11 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a11.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a10'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A10 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a10.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a9'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A9 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a9.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
 }
 function loadiPads(page){
     if (page == 'home'){
         new tableButton('initial')
         header.innerText = 'Pick your iPad:'
         content.innerHTML = '<div include=\'pages/pageContents/tablets.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a15'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A15 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a15.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a14'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A14 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a14.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a13'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A13 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a13.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a12'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A12 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a12.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a11'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A11 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a11.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a10'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A10 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a10.html\'></div>'
+        navBlock.querySelector('.button').style.display = 'none'
+        includeHTML()
+    }
+    if (page == 'a9'){
+        new tableButton('initial')
+        header.innerText = 'Versions Available for A9 Devices'
+        content.innerHTML = '<div include=\'pages/pageContents/hardwares/a9.html\'></div>'
         navBlock.querySelector('.button').style.display = 'none'
         includeHTML()
     }
