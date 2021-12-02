d46d8bc1fdd4219ec82dace89f1ac95ad529e132
Anden Wieseler <ttechno@icloud.com>
2021-12-02 17:35:04 +0000

    test checkra1n loading function

--- jailbreaks.js
+++ jailbreaks.js
@@ -1,14 +1,14 @@
 function loadJB(jailbreak){
     if (jailbreak == 'chkrn'){
-        
+        window.location.href = 'https://yltboi.me/walkthrough/checkra1n.html'
     }
     else if (jailbreak == 'u0'){
-
+        window.location.href = 'https://yltboi.me/walkthrough/unc0ver.html'
     }
     else if (jailbreak == 'Taurine'){
-
+        window.location.href = 'https://yltboi.me/walkthrough/taurine.html'
     }
     else if (jailbreak == 'Odyssey'){
-
+        window.location.href = 'https://yltboi.me/walkthrough/odyssey.html'
     }
 }
